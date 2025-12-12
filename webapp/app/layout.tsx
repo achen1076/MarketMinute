5534e401e4e1c4fa55d781dfac86b764e778f2cd
achen1076 <achen1076@gmail.com>
2025-12-12 04:24:01 -0500

    icon

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -51,6 +51,11 @@ export const metadata: Metadata = {
     address: false,
     telephone: false,
   },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
   openGraph: {
     type: "website",
     locale: "en_US",
